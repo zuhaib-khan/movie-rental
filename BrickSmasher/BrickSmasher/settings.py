99d147a959e7a9011a4fc3e880de8df90ffa74e5
Areeb <areebbb@gmail.com>
2023-05-14 18:14:54 -0500

    add a comment

--- BrickSmasher/BrickSmasher/settings.py
+++ BrickSmasher/BrickSmasher/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'BrickSmasher.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3', # using sqlite3 as db
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
