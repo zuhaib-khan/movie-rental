eb2d8a05af01471f5c15620eb0e77af6e1c06668
Areeb <areebbb@gmail.com>
2023-05-14 21:35:48 -0500

    add placeholders

--- BrickSmasher/app/urls.py
+++ BrickSmasher/app/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("/account", views.index, name="account"),
+    path("/movie", views.index, name="movie"),
+    path("/rent", views.index, name="rent"),
+    path("/dbUser", views.index, name="dbUser"),
+    path("/dbMovie", views.index, name="dbMovie"),
+    path("/dbRent", views.index, name="dbRent"),
 ]
--- BrickSmasher/app/views.py
+++ BrickSmasher/app/views.py
@@ -4,3 +4,26 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def account(request):
+    return HttpResponse("Placeholder for now.")
+
+
+def movie(request):
+    return HttpResponse("Placeholder for now.")
+
+
+def rent(request):
+    return HttpResponse("Placeholder for now.")
+
+def db_user(request):
+    return HttpResponse("Placeholder for now.")
+
+
+def db_movie(request):
+    return HttpResponse("Placeholder for now.")
+
+
+def db_rent(request):
+    return HttpResponse("Placeholder for now.")
